b626b87b45c5c0041bf9c63cc58b46d7bcf08c0f
Flamel-Ferreira <flamelferreira05@hotmail.com>
2022-11-12 08:56:50 -0300

    Alerta sobre Cadastro

---
+++ Js/botao.js
@@ -0,0 +1,3 @@
+function alertaBotão (){
+  alert(`Oi, sou Flamel Ferreira. Essa página é apenas um estudo de design e programação. No momento não é possível se cadastrar! Agradeço a compreensão.`)
+}
